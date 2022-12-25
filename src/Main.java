0b8cc0452680c5b37b7e224cab8540d81a833b0b
Dmitry Kalugin <lcaosl@yandex.ru>
2022-12-25 22:59:16 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public class Money {
-        public static void main(String[] args) {
-            int days = 1000;
-            int speed = 300000;
-            long seconds = days * 24 * 60 * 60;
-            long distance = speed * seconds;
-            System.out.println(distance);
-        }
+
+    public static void main(String[] args) {
+        int days = 1000;
+        int speed = 300000;
+        long seconds = days * 24 * 60 * 60;
+        long distance = speed * seconds;
+        System.out.println(distance);
+
     }
 }
