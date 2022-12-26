4737fbce9c8592d5114f738999ae76f4e6cf0333
Dmitry Kalugin <lcaosl@yandex.ru>
2022-12-26 10:06:41 +0300

    Pythagorean theorem

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
-        int days = 1000;
-        int speed = 300000;
-        long seconds = days * 24 * 60 * 60;
-        long distance = speed * seconds;
-        System.out.println(distance);
-
+        double a = 2;
+        double b = 2;
+        double c2 = a * a + b * b;
+        double c = Math.sqrt(c2);
+        System.out.println(c);
     }
 }
