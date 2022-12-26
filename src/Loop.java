00f69208394907fa28fbdeb75dbcaaeb51674e58
Dmitry Kalugin <lcaosl@yandex.ru>
2022-12-26 23:58:16 +0300

    Loop2

--- src/Loop.java
+++ src/Loop.java
@@ -1,8 +1,8 @@
 public class Loop {
     public static void main(String[] args) {
         int i = 1000;
-        while (i >= 0){
-            if (i % 2 == 0){
+        while (i >= 0) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
             i--;
---
+++ src/Loop2.java
@@ -0,0 +1,11 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        int i = 0;
+        int b = 0;
+        while (i <= 100){
+            b += i;
+            i++;
+        }
+        System.out.println(b);
+    }
+}
