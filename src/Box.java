4fdf2cea96b5526f27c3ffcee787c62b01f44d5c
Dmitry Kalugin <lcaosl@yandex.ru>
2023-01-03 18:27:55 +0300

    Объекты в качестве параметров и возврат объектов

---
+++ src/Box.java
@@ -0,0 +1,28 @@
+public class Box {
+    double a;
+    double b;
+    double c;
+
+    public Box(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    double square() {
+        return a * b * c;
+    }
+
+    void compare(Box box) {
+        double thisSquare = square();
+        double boxSquare = box.square();
+        if (thisSquare > boxSquare) {
+            System.out.println("Больше!");
+        } else if (thisSquare < boxSquare) {
+            System.out.println("Меньше!");
+        } else {
+            System.out.println("Они равны!!!");
+        }
+
+    }
+}
---
+++ src/MainBox.java
@@ -0,0 +1,7 @@
+public class MainBox {
+    public static void main(String[] args) {
+        Box box = new Box(12, 13, 14);
+        Box box1 = new Box(11, 12,13);
+        box.compare(box1);
+    }
+}
